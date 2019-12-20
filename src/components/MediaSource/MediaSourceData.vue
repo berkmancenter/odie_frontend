de2d31d63975cdb065a77139b9ff3289d86bf257
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-12-20 20:06:34 +0100

    Clean MediaSourceData component

--- src/components/MediaSource/MediaSourceData.vue
+++ src/components/MediaSource/MediaSourceData.vue
@@ -114,78 +114,18 @@
 </template>
 
 <script>
-  import _ from 'lodash'
-  import MediaSourceChart from './MediaSourceChart'
   import MediaSourceDataCard from './MediaSourceDataCard'
 
   export default {
     name: 'media-source-data',
     props: ['data'],
     components: {
-      MediaSourceChart,
       MediaSourceDataCard
     },
     data () {
-      return {
-        colorsPalette: [
-          '#F3C300', '#875692', '#F38400', '#A1CAF1', '#BE0032', '#C2B280',
-          '#2B3D26', '#008856', '#E68FAC', '#0067A5', '#F99379', '#604E97',
-          '#F6A600', '#B3446C', '#DCD300', '#882D17', '#8DB600', '#654522',
-          '#E25822', '#E320BC', '#3AF2B8'
-        ],
-        charts: {
-          hashtags: false,
-          top_sources: false,
-          retweets: false,
-          mentions: false,
-          urls: false,
-          words: false
-        },
-        chartsData: {
-          hashtags: {},
-          top_sources: {},
-          retweets: {},
-          mentions: {},
-          urls: {},
-          words: {}
-        }
-      }
+      return {}
     },
-    methods: {
-      sortedSomething (what, howMany = 10) {
-        let asInt = {}
-
-        let limitedObjects = Object.keys(what).slice(0, howMany - 1).reduce((result, key) => {
-          result[key] = what[key]
-          return result
-        }, {})
-
-        _.forEach(limitedObjects, (count, item) => {
-          asInt[item] = parseInt(count)
-        })
-
-        return _(asInt)
-                 .toPairs()
-                 .orderBy([1], ['desc'])
-                 .fromPairs()
-                 .value()
-      },
-      processChartData (data) {
-        return {
-          labels: _.keys(data),
-          datasets: [
-            {
-              backgroundColor: _.take(this.colorsPalette, _.values(data).length),
-              data: _.values(data)
-            }
-          ]
-        }
-      },
-      toggleChart (name) {
-        this.charts[name] = !this.charts[name]
-        this.chartsData[name] = this.processChartData(this.sortedSomething(this.data[name]))
-      }
-    }
+    methods: {}
   }
 </script>
 
